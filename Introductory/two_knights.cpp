805fec3fc16cc26ebf730ce7846e8c688c2b0ab5
Kumar Amitanshu Singh <kumaramitanshu2@gmail.com>
2023-08-25 15:45:39 +0530

    two_knights problem solved

--- Introductory/two_knights.cpp
+++ Introductory/two_knights.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 
 #define ll long long int
-void solve(int n){
+void solve(ll n){
 	if(n == 1){
 		cout << "0" <<endl;
 		return;
 	}
-	
+	ll total_space = ((n * n)*(n*n - 1))/2;
+	ll attack = 4 * (n-1)*(n-2);
+
+	if(attack > 0){
+		cout << total_space - attack << endl;
+		return;
+	}
+	cout << total_space << endl;
 }
 
 int main(){
@@ -15,7 +22,8 @@ int main(){
 	int n;
 	cin >> n;
 	for(int i = 1; i<=n; i++){
-		solve(i);
+		ll n = 1LL*i;
+		solve(n);
 	}
 
 	return 0;
